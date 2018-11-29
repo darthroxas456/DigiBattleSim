41422d7c3ea7fcd45ef2e4f308fc647eca73e231
Owen Rose <ojrose8935@eagle.fgcu.edu>
2018-11-29 10:12:54 -0500

    added for java project

---
+++ DigimonBattleSimulator/src/Search_Array_For_Value.java
@@ -0,0 +1,43 @@
+/**
+ * This file is used to find a value in a single array and to find the smallest value in an array.
+ * 
+ * @author Owen Rose
+ */
+public class Search_Array_For_Value {
+  public static void main(String[] args) {
+
+    int[] scores = {87, 75, 98, 100, 82, 72, 88, 92, 60, 78};
+
+    boolean found;
+
+    int index;
+
+    found = false;
+
+    
+    index = 0;
+
+    while (found == false && index < scores.length) {
+      if (scores[index] == 100) {
+        found = true;
+      }else {
+        index = index + 1;
+      }
+    }
+    
+    if (found) {
+      System.out.println("You earned 100 on test number " + (index + 1));
+    }else {
+      System.out.println("You did not earn 100 on any test.");
+    }
+    
+    int minNumber = 100;
+    
+    for(index = 0; index < scores.length; index++) {
+      if (scores[index] < minNumber) {
+        minNumber = scores[index];
+      }
+    }
+   System.out.println(minNumber + " is the lowset test score.");
+  }
+}
